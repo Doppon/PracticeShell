847a460634f9de0b42a21da6eeb0b85c747e4243
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-18 14:02:32 +0900

    ファイル入出力のシステムコールのコードを追加

---
+++ Lesson4/kadai4/kadai4_system_call.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+main(){
+  char c;
+  int src, dst;
+  int count;
+
+  src = open("src.txt", O_RDONLY);
+  if(src < 0){
+    perror("src");
+    close(src);
+    exit(1);
+  }
+
+  dst = open("dst.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
+  if(dst < 0){
+    perror("dst");
+    close(dst);
+    exit(1);
+  }
+
+  while((count = read(src, &c, 1)) > 0){
+    if(write(dst, &c, count) < 0){
+      perror("write");
+      exit(1);
+    }
+  }
+
+  if(count < 0){
+    perror("read");
+    exit(1);
+  }
+
+  close(src);
+  close(dst);
+}
+
