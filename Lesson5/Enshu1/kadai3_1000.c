c041c6608460e44d3cb3393d4fdd5f8bd2a73c4c
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-31 17:52:02 +0900

    プロセス生成プログラムを追加

---
+++ Lesson5/Enshu1/kadai3_1000.c
@@ -0,0 +1,25 @@
+#include <stdio.h>   /* printf, stderr, fprintf */
+#include <sys/types.h> /* pid_t */
+#include <unistd.h>  /* _exit, fork */
+#include <stdlib.h>  /* exit */
+#include <errno.h>   /* errno */
+
+int main(void){
+   pid_t  pid;
+
+   pid = fork();
+   if (pid == -1){
+      fprintf(stderr, "can't fork, error %d\n", errno);
+      exit(EXIT_FAILURE);
+   }
+
+   if (pid == 0){
+      int j;
+      for (j = 0; j <= 1000; j++){
+         printf("child: %d\n", j);
+      }
+      _exit(0);
+   }
+   
+   return 0;
+}
