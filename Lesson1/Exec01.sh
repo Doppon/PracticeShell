b334ada7e48e1e12fe32b62c51cdb97cb98e3d79
Doppon <zaxpkuraudo7777@gmail.com>
2019-09-30 09:32:25 +0900

    拡張子を設定

--- Lesson1/Exec01.sh
+++ Lesson1/Exec01.sh
@@ -7,7 +7,7 @@ FILE_NAME="Exec01_file"
 while [ $COUNT -ne 3 ]
 do
   COUNT=`expr ${COUNT} + 1`
-  touch "${FILE_NAME}_${COUNT}"
-  echo "Just create file name: ${FILE_NAME}_${COUNT}"
-  sleep 0.5
+  touch "${FILE_NAME}_${COUNT}.c"
+  echo "Just create file name: ${FILE_NAME}_${COUNT}.c"
+  sleep 2
 done
