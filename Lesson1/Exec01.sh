9a89717e6b095df6795cb8b1be1f426fc2fd12e1
Doppon <zaxpkuraudo7777@gmail.com>
2019-09-30 09:15:37 +0900

    ファイル名が変わったので実行内容も修正

--- Lesson1/Exec01.sh
+++ Lesson1/Exec01.sh
@@ -1,5 +1,5 @@
-mkdir kadai1_dir
-cd kadai1_dir
-touch kadai1_file1
-touch kadai1_file2
-touch kadai1_file3
+mkdir Exec01_dir
+cd Exec01_dir
+touch Exec01_file1
+touch Exec01_file2
+touch Exec01_file3
