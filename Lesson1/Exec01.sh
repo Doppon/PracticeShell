c18f78a776b715600f29bdfb2811176020934c33
Doppon <zaxpkuraudo7777@gmail.com>
2019-09-30 10:05:20 +0900

    Add backup function

--- Lesson1/Exec01.sh
+++ Lesson1/Exec01.sh
@@ -9,5 +9,11 @@ do
   COUNT=`expr ${COUNT} + 1`
   touch "${FILE_NAME}_${COUNT}.c"
   echo "Just create file name: ${FILE_NAME}_${COUNT}.c"
+done
+
+for file in *.c
+do
+  cp $file $file.bak
+  echo "Just Complete backup: ${file}"
   sleep 2
 done
