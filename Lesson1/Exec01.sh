f5b981c889fc4e5d24f2c08aa537cc47c5215ee2
Doppon <zaxpkuraudo7777@gmail.com>
2019-09-30 09:26:04 +0900

    Add base program for kadai1

--- Lesson1/Exec01.sh
+++ Lesson1/Exec01.sh
@@ -1,5 +1,11 @@
 mkdir Exec01_dir
 cd Exec01_dir
-touch Exec01_file1
-touch Exec01_file2
-touch Exec01_file3
+
+COUNT=0
+while [ $COUNT -ne 3 ]
+do
+  COUNT=`expr ${COUNT} + 1`
+  touch Exec01_file
+  echo "Just create file name: Exec01_file${COUNT}"
+  sleep 0.5
+done
