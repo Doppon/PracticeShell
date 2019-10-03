a860485bd81ab16665dbdd990c3b64b440f841e5
Doppon <zaxpkuraudo7777@gmail.com>
2019-10-04 08:11:03 +0900

    ロギングする機能を追加した課題2を追加

---
+++ Lesson1/Exec02.sh
@@ -0,0 +1,25 @@
+COUNT=0
+FILE_NAME="Exec01_file"
+DIR_NAME="Exec01_dir"
+
+mkdir $DIR_NAME
+cd $DIR_NAME
+
+while [ $COUNT -ne 3 ]
+do
+  COUNT=`expr ${COUNT} + 1`
+  touch "${FILE_NAME}_${COUNT}.c"
+  echo "$(date '+%Y-%m-%dT%H:%M:%S') Create file name: ${FILE_NAME}_${COUNT}.c"
+done
+
+for file in *.c
+do
+  BACKUP_COUNT=0
+  while [ $BACKUP_COUNT -ne 5 ]
+  do
+    BACKUP_COUNT=`expr ${BACKUP_COUNT} + 1`
+    cp $file "${file}.bak${BACKUP_COUNT}"
+    echo "$(date '+%Y-%m-%dT%H:%M:%S') Complete backup: ${file}.bak${BACKUP_COUNT}"
+    sleep 2
+  done
+done
