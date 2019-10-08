1194e959445b0587e0bc34e2d66a11943c512152
Doppon <zaxpkuraudo7777@gmail.com>
2019-10-08 14:04:53 +0900

    1以上と1未満で条件分岐を追加

--- Lesson2/mypower.c
+++ Lesson2/mypower.c
@@ -4,5 +4,14 @@ int main(int argc, char const *argv[])
   int input = atoi(argv[1]);
   printf("入力された数値は: %d \n", input);
 
+  if (input >= 1)
+  {
+    printf("1以上");
+  } else
+  {
+    printf("1未満");
+  }
+  
+  
   return 0;
 }
