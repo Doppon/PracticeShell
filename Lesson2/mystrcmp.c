a2989ba812a8082aea517f399ac670ddbe9f3128
Doppon <zaxpkuraudo7777@gmail.com>
2019-10-10 16:19:47 +0900

    mystrcmpの異常系を追加

---
+++ Lesson2/mystrcmp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+  if (argv[1] == NULL)
+  {
+    printf("Usage: strcmp str str");
+    return 0;
+  }
+
+  return 0;
+}
