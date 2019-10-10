3f3f8354d592452522b36888893afa100b5950e3
Doppon <zaxpkuraudo7777@gmail.com>
2019-10-10 16:53:59 +0900

    argvから受け取った文字列ポインタの長さを測るための関数を追加

--- Lesson2/mystrcmp.c
+++ Lesson2/mystrcmp.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int LengthOf(const char *s)
+{
+  int n = 0;
+
+  while (*s++ != '\0')
+    n++;
+  return (n);
+}
+
 int main(int argc, char const *argv[])
 {
   if (argv[1] == NULL)
