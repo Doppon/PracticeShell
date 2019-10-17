9160c7c96a47700d35f5a57e01f48dcc52acb38f
Doppon <zaxpkuraudo7777@gmail.com>
2019-10-17 20:09:45 +0900

    fgetsで得られた改行を優先して出力側の改行を削除

---
+++ Lesson3/str_kadai1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define LINE_LEN 80
+
+int main(void)
+{
+  char line_buf[LINE_LEN];
+  while (fgets(line_buf, LINE_LEN, stdin) != NULL)
+    // puts(line_buf);
+    printf("%s", line_buf);
+
+}
