feb140bf926a423105736823e43393a7f812371d
Doppon <zaxpkuraudo7777@gmail.com>
2020-01-16 15:17:08 +0900

    コードのリファクタリング

--- 2019/Lesson3/src/str_kadai2.c
+++ 2019/Lesson3/src/str_kadai2.c
@@ -7,9 +7,13 @@ int main()
 
   while ((c = getchar()) != EOF) {
     if (islower(c))
+    {
       c = toupper(c);
+    } 
     else if (isupper(c))
+    {
       c = tolower(c);
+    }
     putchar(c);
   }
 }
