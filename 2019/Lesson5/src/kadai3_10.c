c70f5632904f865f4b484c61b0843228efaf3a7f
Doppon <zaxpkuraudo7777@gmail.com>
2020-01-18 15:17:51 +0900

    フォーマットのリファクタリング

--- 2019/Lesson5/src/kadai3_10.c
+++ 2019/Lesson5/src/kadai3_10.c
@@ -5,23 +5,24 @@
 #include <errno.h>     /* errno */
 
 int main(void){
-   pid_t  pid;
+  pid_t  pid;
 
-   pid = fork();
-   if (pid == -1){
-      fprintf(stderr, "can't fork, error %d\n", errno);
-      // Define EXIT_FAILURE 1 for stdlib.h
-      exit(EXIT_FAILURE);
-   }
+  pid = fork();
+  if (pid == -1)
+  {
+    fprintf(stderr, "can't fork, error %d\n", errno);
+    // Define EXIT_FAILURE 1 for stdlib.h
+    exit(EXIT_FAILURE);
+  }
 
-   if (pid == 0){
-      int j;
-      for (j = 0; j <= 10; j++){
-         printf("child: %d\n", j);
-      }
-      _exit(0);
-   }
-   
-   return 0;
+  if (pid == 0)
+  {
+    int j;
+    for (j = 0; j <= 10; j++){
+      printf("child: %d\n", j);
+    }
+    _exit(0);
+  }
+  return 0;
 }
 
