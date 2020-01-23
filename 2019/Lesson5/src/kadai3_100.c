924a08309588826ddc173c2e39b42c82bd98ba3d
Doppon <zaxpkuraudo7777@gmail.com>
2020-01-23 16:02:46 +0900

    100個生成するプログラムを10個のものから拡張

--- 2019/Lesson5/src/kadai3_100.c
+++ 2019/Lesson5/src/kadai3_100.c
@@ -1,25 +1,55 @@
-#include <stdio.h>   /* printf, stderr, fprintf */
+#include <stdio.h>     /* printf, stderr, fprintf */
 #include <sys/types.h> /* pid_t */
-#include <unistd.h>  /* _exit, fork */
-#include <stdlib.h>  /* exit */
-#include <errno.h>   /* errno */
+#include <unistd.h>    /* _exit, fork, getpid */
+#include <stdlib.h>    /* exit */
+#include <errno.h>     /* errno */
 
-int main(void){
-   pid_t  pid;
+#define P_MAX 100
 
-   pid = fork();
-   if (pid == -1){
-      fprintf(stderr, "can't fork, error %d\n", errno);
-      exit(EXIT_FAILURE);
-   }
+int main(){
+  int count, val;
+	int pid[P_MAX];
 
-   if (pid == 0){
-      int j;
-      for (j = 0; j <= 100; j++){
-         printf("child: %d\n", j);
-      }
-      _exit(0);
-   }
-   
-   return 0;
+	// 子プロセス生成。子プロセスは次の行から始まるため、
+	// このような記述をすると、子プロセスが子プロセスを生成しないで済む。
+	// for( count=0 ; count < P_MAX && (pid[count] = fork()) > 0 ; count++ );
+  count = 0;
+  while (count < P_MAX)
+  {
+    // fork()
+    //
+    // Success :
+    //   Parent Pid => Child Pid
+    //   Child  Pid => 0
+    // Failed  :
+    //   Parent Pid => -1
+    //   Child  Pid => errno
+    if ((pid[count] = fork()) > 0)
+    {
+      count++;
+    } else
+    {
+      break;
+    }
+  }
+
+  //親プロセスはすべての子プロセスの終了を待つ
+	if( count == P_MAX ){
+		for( count = 0 ; count < P_MAX ; count++ )
+    {
+			wait(&val);
+		}
+  // 子プロセス
+	} else if( pid[count] == 0)
+  {
+		printf("child:%d\n",count);
+    printf("This is child. (pid=%d)\n", getpid());
+		exit(0);
+	} else
+  {
+    perror("child process") ;
+    exit(0);
+	}
+	return 0;
 }
+
